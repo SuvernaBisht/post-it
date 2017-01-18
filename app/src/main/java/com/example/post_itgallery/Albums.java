905499e2783cb096421794f6b30ae22ae703bfb2
SuvernaBisht <suvernabisht97@gmail.com>
2017-01-18 19:51:55 +0530

    My comit message2

--- app/src/main/java/com/example/post_itgallery/Albums.java
+++ app/src/main/java/com/example/post_itgallery/Albums.java
@@ -3,8 +3,11 @@ package com.example.post_itgallery;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class Albums extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/post_itgallery/Photos.java
+++ app/src/main/java/com/example/post_itgallery/Photos.java
@@ -29,7 +29,7 @@ public class Photos extends Activity {
     private ArrayList<ImageItem> getData()
     {
         final ArrayList<ImageItem> imageItems=new ArrayList<>();
-        TypedArray imgs=getResources().obtainTypedArray(R.array.image_ids);
+        TypedArray imgs=getResources().obtainTypedArray(R.array.image_id);
         for (int i=0;i<imgs.length();i++)
         {
             Bitmap bitmap= BitmapFactory.decodeResource(getResources(),imgs.getResourceId(i, -1));
